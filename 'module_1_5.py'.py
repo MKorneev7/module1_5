55c12c4ab9dac536aab00d5adc4def2adc7f389a
MKorneev7 <mik61349304@yandex.ru>
2024-08-29 22:48:26 +0300

    Add files via upload

---
+++ 'module_1_5.py'.py
@@ -0,0 +1,8 @@
+immutable_var= tuple[1, 2, 'a', 'b']
+print(immutable_var)
+immutable_var [1]=10
+print(immutable_var)
+mutable_list = [3, 4, 'b', 'c']
+print(mutable_list)
+mutable_list [1] =9
+print(mutable_list)
